95b43d1d6b83ed41337cbe71ecf86674895f7b61
Cleiton-Gaioti <cleiton.s.gaioti@gmail.com>
2022-03-20 20:44:45 -0300

    tirando log

--- src/main/java/com/pss/senha/validacao/ValidadorSenha.java
+++ src/main/java/com/pss/senha/validacao/ValidadorSenha.java
@@ -3,14 +3,10 @@ package com.pss.senha.validacao;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 
 public final class ValidadorSenha {
 
     private final ArrayList<TratadorSenha> tratadores = new ArrayList<>();
-    private static final Logger logger = Logger.getLogger(ValidadorSenha.class);
 
     public ValidadorSenha() {
 
@@ -25,9 +21,6 @@ public final class ValidadorSenha {
         tratadores.add(new CaseSensitiveTratador(false));
         tratadores.add(new LetrasAcentuadasTratador());
         tratadores.add(new WhiteSpaceTratador());
-        
-        BasicConfigurator.configure();
-        Logger.getRootLogger().setLevel(Level.INFO);
 
     }
 
@@ -47,7 +40,6 @@ public final class ValidadorSenha {
             String resultado = tratador.verificar(senha);
             if (!resultado.isEmpty()) {
                 resultadosValidacao.add(resultado);
-                logger.info(resultado);
             }
         }
 
